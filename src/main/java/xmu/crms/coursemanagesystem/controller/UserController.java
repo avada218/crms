32ac1f5979854c73407664529da7f1235f78ea05
badcode <mjp1753@163.com>
2017-11-29 12:34:08 +0800

    For security

---
+++ src/main/java/xmu/crms/coursemanagesystem/controller/UserController.java
@@ -0,0 +1,8 @@
+package xmu.crms.coursemanagesystem.controller;
+
+/**
+ * @author badcode
+ * @date 2017/11/27
+ */
+public class UserController {
+}
---
+++ src/main/java/xmu/crms/coursemanagesystem/entity/School.java
@@ -0,0 +1,8 @@
+package xmu.crms.coursemanagesystem.entity;
+
+/**
+ * @author badcode
+ * @date 2017/11/27
+ */
+public class School {
+}
