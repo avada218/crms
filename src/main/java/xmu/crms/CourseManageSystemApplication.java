aefd6a1aef0c24a5083715e053546b34cb0080d1
badcode <mjp1753@163.com>
2017-12-24 11:38:39 +0800

    add MapperScan

--- src/main/java/xmu/crms/CourseManageSystemApplication.java
+++ src/main/java/xmu/crms/CourseManageSystemApplication.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +19,7 @@ import java.text.SimpleDateFormat;
  * @date 2017/12/01
  */
 @SpringBootApplication
+@MapperScan(basePackages = "xmu.crms.mapper")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CourseManageSystemApplication {
 
