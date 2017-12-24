53a8be2c87ef05ab8b72bc31f2e81be8fe0e010f
badcode <mjp1753@163.com>
2017-12-24 11:36:22 +0800

    add UserService and TimerService

--- src/main/java/xmu/crms/service/TimerService.java
+++ src/main/java/xmu/crms/service/TimerService.java
@@ -4,11 +4,13 @@ import java.math.BigInteger;
 import java.util.Date;
 import java.util.HashMap;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 /**
  * 定时器
  * 
  * @author qinlingyun liuaiqi
- * @version 2.10
+ * @version 2.20
  */
 public interface TimerService {
 
@@ -26,18 +28,17 @@ public interface TimerService {
 	/**
 	 * 更新Event表.
 	 * @author qinlingyun
-	 * @param time 事件的时间
-	 * @param beanName 对象名
-	 * @param methodName 方法名
-	 * @param paramMap 方法参数
+	 * @param eventId 事件的ID
+	 * @param newTime 需要修改的时间
 	 */	 
-	 void updateEvent(Date time, String beanName, String methodName, HashMap<BigInteger, String> paramMap);
+	 void updateEvent(BigInteger eventId, Date newTime);
 
 	 
 	/**
 	 * 每十分钟检查一次Event实体的状况
 	 * @author qinlingyun
 	 */
+	 @Scheduled(fixedRate = 1000*60*10)
 	 void scheduled();
 		
 }
--- src/main/resources/static/js/student/StudentBindPage.js
+++ src/main/resources/static/js/student/StudentBindPage.js
@@ -1,54 +1,88 @@
 $(function () {
     $.ajax({
-        url: "/school/province",
+        url: "http://apis.map.qq.com/ws/district/v1/list?key=7DFBZ-K4PWQ-TYK5Y-GL7XN-RBDSQ-XSB6M&output=jsonp",
         type: "GET",
+        dataType: "JSONP",
         success: function (data) {
-            for(var i = 0;i<data.length;i++)
+            var result = data.result[0];
+            for(var i = 0;i<result.length;i++)
                 $("#province").append(
-                    "<option value=\"" + data[i] + "\">" + data[i] + "</option>"
+                    "<option value=\"" + result[i].name + "\" id=\"" + result[i].id + "\">" + result[i].name + "</option>"
                 );
             $.ajax({
-                url: "/school/city?province=" + $("#province option:selected").attr("value"),
+                url: "http://apis.map.qq.com/ws/district/v1/getchildren?key=7DFBZ-K4PWQ-TYK5Y-GL7XN-RBDSQ-XSB6M&output=jsonp&id=" + $("#province option:selected").attr("id"),
                 type: "GET",
+                dataType: "JSONP",
                 success: function (data) {
-                    for(var i = 0;i<data.length;i++)
+                    var result = data.result[0];
+                    for (var i = 0; i < result.length; i++)
                         $("#city").append(
-                            "<option value=\"" + data[i] + "\">" + data[i] + "</option>"
+                            "<option value=\"" + result[i].name + "\" id=\"" + result[i].id + "\">" + result[i].name + "</option>"
                         );
+                    console.log($("#city option:selected").attr("value"));
                     $.ajax({
                         url: "/school?city=" + $("#city option:selected").attr("value"),
                         type: "GET",
                         success: function (data) {
-                            for(var i = 0;i<data.length;i++)
+                            for (var i = 0; i < data.length; i++)
                                 $("#school").append(
-                                    "<option value=\"" + data[i].name + "\">" + data[i].name + "</option>"
+                                    "<option value=\"" + data[i].name + "\" id=\"" + data[i].id + "\">" + data[i].name + "</option>"
                                 );
                         }
-                    });
+                    })
                 }
             });
         }
     });
 
+    $("#province").change(function () {
+        $("#city").empty();
+        $.ajax({
+            url: "http://apis.map.qq.com/ws/district/v1/getchildren?key=7DFBZ-K4PWQ-TYK5Y-GL7XN-RBDSQ-XSB6M&output=jsonp&id=" + $("#province option:selected").attr("id"),
+            type: "GET",
+            dataType: "JSONP",
+            success: function (data) {
+                var result = data.result[0];
+                for(var i = 0;i<result.length;i++)
+                    $("#city").append(
+                        "<option value=\"" + result[i].name + "\" id=\"" + result[i].id + "\">" + result[i].name + "</option>"
+                    );
+            }
+        });
+    });
+
+    $("#city").change(function () {
+        $("#school").empty();
+        $.ajax({
+            url: "/school?city=" + $("#city option:selected").attr("value"),
+            type: "GET",
+            success: function (data) {
+                for (i = 0; i < data.length; i++) {
+                    $("#school").append(
+                        "<option value=\"" + data[0].name + "\" id=\"" + data[0].id + "\">" + data[0].name + "</option>"
+                    );
+                }
+            }
+        });
+    });
+
     $("#submit").on("click", function() {
-        teacherInfo = {
+        studentInfo = {
             name: $("#name").val(),
             phone: $("#phoneNum").val(),
             password: $("#password").val(),
-            gender: $("input[name=gender]:checked").val() == "男" ? "male" : "female",
+            gender: $("input[name=gender]:checked").val() == "男" ? 0 : 1,
             number: $("#stuffNum").val(),
             email: $("#eMail").val(),
             school: {
-                province: $("#province option:selected").attr("value"),
-                city: $("#city option:selected").attr("value"),
-                name: $("#school option:selected").attr("value")
+                id: $("#school option:selected").attr("id")
             }
         };
         $.ajax({
             url: "/me",
             type: "PUT",
             contentType: "application/json",
-            data: JSON.stringify(teacherInfo),
+            data: JSON.stringify(studentInfo),
             success: function () {
                 alert("绑定信息成功!");
                 window.location.href = "/student/home";
